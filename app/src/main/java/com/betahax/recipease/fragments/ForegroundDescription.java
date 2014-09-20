33ab0e7531daaa5804ed9a470e51f9a2a2d92ecc
Andrew <aschuster3@gatech.edu>
2014-09-20 05:44:56 -0400

    Updated BackgroundImage and ForegroundDescription in prep for parallax effect

--- app/src/main/java/com/betahax/recipease/fragments/ForegroundDescription.java
+++ app/src/main/java/com/betahax/recipease/fragments/ForegroundDescription.java
@@ -1,51 +1,48 @@
 package com.betahax.recipease.fragments;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.betahax.recipease.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ForegroundDescription.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ForegroundDescription#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
+import java.util.ArrayList;
+
 public class ForegroundDescription extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
+    private static final String RECIPE_NAME = "name";
+    private static final String INGREDIENTS = "ingred";
+    private static final String DIRECTIONS = "dir";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private String recipeName;
+    private ArrayList<String> ingredients;
+    private String directions;
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param recipeName The name of the recipe.
+     * @param ingredientsRaw A raw JSONArray of all the ingredients in a recipe.
+     * @param directions The directions to prepare the recipe.
+     *
      * @return A new instance of fragment ForegroundDescription.
      */
     // TODO: Rename and change types and number of parameters
-    public static ForegroundDescription newInstance(String param1, String param2) {
+    public static ForegroundDescription newInstance(String recipeName, String ingredientsRaw, String directions) {
         ForegroundDescription fragment = new ForegroundDescription();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(RECIPE_NAME, recipeName);
+        args.putString(INGREDIENTS, ingredientsRaw);
+        args.putString(DIRECTIONS, directions);
         fragment.setArguments(args);
         return fragment;
     }
@@ -57,55 +54,45 @@ public class ForegroundDescription extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            recipeName = getArguments().getString(RECIPE_NAME);
+            ingredients = jsonArrayToStringArray(getArguments().getString(INGREDIENTS));
+            directions = getArguments().getString(DIRECTIONS);
         }
+    }
+
+    private ArrayList<String> jsonArrayToStringArray(String rawJSON) {
+        ArrayList<String> ingredients = new ArrayList<String>();
+
+        try {
+            JSONArray arr = new JSONArray(rawJSON);
+
+            for(int i = 0; i < arr.length(); i++) {
+                ingredients.add(arr.getString(i));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return ingredients;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_foreground_description, container, false);
+        View view = inflater.inflate(R.layout.fragment_foreground_description, container, false);
+
+        ((TextView) view.findViewById(R.id.recipe_name)).setText(recipeName);
+        generateIngredientsView(((LinearLayout) view.findViewById(R.id.list_of_ingredients)), ingredients);
+        ((TextView) view.findViewById(R.id.directions_body)).setText(directions);
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+    private void generateIngredientsView(LinearLayout layout, ArrayList<String> ingredients) {
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        // TODO populate ingredients layout
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
     }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
-
 }
