2b84e69a3fff19ca8b2031198f40acf932987eb6
Andrew <aschuster3@gatech.edu>
2014-09-20 22:08:51 -0400

    Laid recipe database groundwork

--- app/src/main/java/com/betahax/recipease/api/DatabaseReader.java
+++ app/src/main/java/com/betahax/recipease/api/DatabaseReader.java
@@ -1,10 +1,15 @@
 package com.betahax.recipease.api;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.betahax.recipease.database.RecipeDBHelper;
+import com.betahax.recipease.database.contracts.RecipeContract;
 import com.betahax.recipease.model.Recipe;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -14,9 +19,53 @@ import java.util.ArrayList;
 public class DatabaseReader {
 
     public static ArrayList<Recipe> fetchSavedRecipes(Context context) {
+        ArrayList<Recipe> recipes = new ArrayList<Recipe>();
         SQLiteDatabase db = new RecipeDBHelper(context).getReadableDatabase();
 
+        Cursor c = db.rawQuery(RecipeContract.FETCH_RECIPES, null);
 
-        return null;
+        c.moveToFirst();
+
+        int colId = c.getColumnIndex(RecipeContract.DBEntry._ID),
+            colRecipeName = c.getColumnIndex(RecipeContract.DBEntry.RECIPE_NAME),
+            colIngredients = c.getColumnIndex(RecipeContract.DBEntry.INGREDIENTS),
+            colImageSrc = c.getColumnIndex(RecipeContract.DBEntry.IMAGE_SOURCE),
+            colRecipeDirections = c.getColumnIndex(RecipeContract.DBEntry.RECIPE_DIRECTIONS),
+            colNotes = c.getColumnIndex(RecipeContract.DBEntry.NOTES);
+
+        int size = c.getCount();
+        Recipe r;
+        for(int i = 0; i < size; i++) {
+            r = new Recipe(c.getString(colId), c.getString(colRecipeName), jsonArrayToStringArray(c.getString(colIngredients)),
+                    c.getString(colImageSrc), c.getString(colNotes), c.getString(colRecipeDirections));
+
+            recipes.add(r);
+        }
+        return recipes;
+    }
+
+    private static ArrayList<String> jsonArrayToStringArray(String rawJSON) {
+        ArrayList<String> ingredients = new ArrayList<String>();
+
+        try {
+            JSONArray arr = new JSONArray(rawJSON);
+
+            for(int i = 0; i < arr.length(); i++) {
+                ingredients.add(arr.getString(i));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return ingredients;
+    }
+
+    public static void saveRecipe(Context context, String recipeName, String ingredients, String imageSrc) {
+        SQLiteDatabase db = new RecipeDBHelper(context).getWritableDatabase();
+
+        // TODO query api for recipe information and other jazzy information
+
+        //db.rawQuery(RecipeContract.putRecipe())
     }
 }
--- app/src/main/java/com/betahax/recipease/database/contracts/RecipeContract.java
+++ app/src/main/java/com/betahax/recipease/database/contracts/RecipeContract.java
@@ -17,6 +17,7 @@ public class RecipeContract extends GeneralContract {
                                    RECIPE_NAME = "name",
                                    INGREDIENTS = "ingredients",
                                    IMAGE_SOURCE = "image_source",
+                                   RECIPE_DIRECTIONS = "directions",
                                    NOTES = "notes";
     }
 
@@ -26,8 +27,17 @@ public class RecipeContract extends GeneralContract {
         DBEntry.RECIPE_NAME + TEXT_TYPE + COMMA_SEP +
         DBEntry.INGREDIENTS + TEXT_TYPE + COMMA_SEP +
         DBEntry.IMAGE_SOURCE + TEXT_TYPE + COMMA_SEP +
+        DBEntry.RECIPE_DIRECTIONS + TEXT_TYPE + COMMA_SEP +
         DBEntry.NOTES + TEXT_TYPE + COMMA_SEP +
         ")";
+
+    public static final String FETCH_RECIPES =
+        "SELECT * FROM " + DBEntry.TABLE_NAME;
+
+    public static String putRecipe(String id, String name, String jsonArray, String imgSrc, String directions) {
+        return "INSERT INTO " + DBEntry.TABLE_NAME + " VALUES(" + id + COMMA_SEP +
+                name + COMMA_SEP + jsonArray + COMMA_SEP + imgSrc + COMMA_SEP + directions + COMMA_SEP + " )";
+    }
 
     public static final String DROP_TABLE =
         "DROP TABLE IF EXISTS " + DBEntry.TABLE_NAME;
